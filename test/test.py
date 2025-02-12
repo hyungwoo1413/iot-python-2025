d7fa5c762749106a95bff664e1c5332f7955df11
hyungwoo1413 <glhwguddn@gmail.com>
2025-02-12 09:08:34 +0900

    test

--- test/test.py
+++ test/test.py
@@ -2,30 +2,23 @@ from tkinter import *
 import random
 rows = 3
 cols = 3
+    
+def buttonClick():
+    if(btn == None):
+        print('')
+
+root = Tk()
+
 arr = [[0 for _ in range(rows)] for _ in range(cols)]
 num = random.sample(range(1,rows*cols+1),rows*cols)
 k = 0
-
-def buttonClick():
-    for i in range(rows):
-        for j in range(cols):
-            if(arr[i][j] == None):
-                arr[i][j] = arr[i][j+1]
-                arr[i][j+1] = None
-
-
-root = Tk()
 
 num[num.index(rows*cols)] = None
 
 for i in range(rows):
     for j in range(cols):
         arr[i][j] = num[k]
-        Button(root, text=arr[i][j], width=20, height=9, command=buttonClick).grid(row=i, column=j)
+        btn = Button(root, text=arr[i][j], width=20, height=9, command=buttonClick).grid(row=i, column=j)
         k += 1
 
 root.mainloop()
-
-# [ , 2, 3]
-# [4, 5, 6]
-# [7, 8, 9]
